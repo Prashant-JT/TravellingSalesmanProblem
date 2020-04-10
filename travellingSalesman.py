d378e26ed6d7a5670867cb23d11ab0297cffd6ed
Prashant-JT <55165517+Prashant-JT@users.noreply.github.com>
2020-04-10 05:38:11 +0100

    Add files via upload

--- travellingSalesman.py
+++ travellingSalesman.py
@@ -2,18 +2,39 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
 import math
+import networkx as nx
+import matplotlib.pyplot as plt
 from collections import namedtuple
+from IPython.display import FileLink
+import csv
+
+def submission_generation(filename, str_output):
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        for item in str_output:
+            writer.writerow(item)
+    return FileLink(filename)
 
 Point = namedtuple("Point", ['x', 'y'])
 
 def length(point1, point2):
     return math.sqrt((point1.x - point2.x)**2 + (point1.y - point2.y)**2)
 
-def solve_it(input_data):
-    # Modify this code to run your optimization algorith
-    # parse the input
-    lines = input_data.split('\n')
+def check_solution(obj):
+    # comprobar que todos los nodos son distintos (all different), recorrer la lista
+    # sumando los pesos y que acabe en la misma ciudad en la que empezó
+    if True:
+        return obj
+    return null
 
+def printGraph(g):
+    print(nx.info(g))
+    nx.draw(g, with_labels=True)
+    plt.show()
+
+def solve_it(input_data):
+    """Christofides algortithm"""
+    lines = input_data.split('\n')
     nodeCount = int(lines[0])
 
     points = []
@@ -22,7 +43,6 @@ def solve_it(input_data):
         parts = line.split()
         points.append(Point(float(parts[0]), float(parts[1])))
 
-    # build a trivial solution
     # visit the nodes in the order they appear in the file
     solution = range(0, nodeCount)
 
@@ -35,7 +55,24 @@ def solve_it(input_data):
     output_data = '%.2f' % obj + ' ' + str(0) + '\n'
     output_data += ' '.join(map(str, solution))
 
-    return output_data, obj
+    # create a graph k-complete
+    G = nx.complete_graph(nodeCount, create_using=nx.Graph())
+    printGraph(G)
+
+    # obtain the minimum spanning tree (prim is good for a lot of edges)
+    T = nx.minimum_spanning_tree(G, weight='None', algorithm='prim', ignore_nan=False)
+    printGraph(T)
+
+    # seperate nodes with odd degree and get perfect matching
+    W = {}
+    for i in range(0, nodeCount - 1):
+        print(i, T.degree[i])
+        if T.degree[i] % 2 != 0:
+            W[i] = T.degree[i]
+    #M = nx.is_perfect_matching(G, matching=W)
+    #print(W)
+
+    return output_data, check_solution(obj)
 
 if __name__ == "__main__":
     for dirname, _, filenames in os.walk('data'):
@@ -48,5 +85,8 @@ if __name__ == "__main__":
             with open(full_name, 'r') as input_data_file:
                 input_data = input_data_file.read()
                 output, value = solve_it(input_data)
-                str_output.append([filename,str(value)])
-    print(str_output)
+                str_output.append([filename, str(value)])
+    submission_generation('sample_submission_non_sorted.csv', str_output)
+    reader = csv.reader(open("sample_submission_non_sorted.csv"))
+    sortedlist = sorted(reader, key=lambda row: row[0], reverse=False)
+    submission_generation('christofides.csv', sortedlist)
